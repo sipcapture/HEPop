185f02a6f93d39e0c9ebb5c7bc97615fc5f1a77a
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-02-11 14:41:01 +0100

    Update es-broker.js

--- src/es-broker.js
+++ src/es-broker.js
@@ -2,6 +2,24 @@
  * es-broker.js
  * ISC Licensed
  * Based on https://github.com/ejjoo/node-elasticsearch-broker
+ *  
+ *  #Create
+ *  var EsBroker = require('./es-broker');
+ *  var esBroker = EsBroker.create({
+ *    parser: function, //user data parser which will be parsed in job_queue
+ *    target: string, //elasticsearch host target
+ *    max_bulk_qtty: number, //maximum bulk quantity sent to elasticsearch at once  
+ *    max_request_num: number, //maximum request number concurrently.
+ *    verbose: boolean, //verbose
+ *    index: string //elastic_search index
+ *  }};
+ *
+ * #Push
+ *  esBroker.push(string);
+ * 
+ * # Close
+ *  esBroker.close(callback);
+ *
  */
 
 const log = require('./logger');
