c9308907c3b836791febf3d837468b16c8ceab9a
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-03-10 13:47:47 +0100

    update dev

--- src/hepcore.js
+++ src/hepcore.js
@@ -4,15 +4,17 @@ const sip = require('sip');
 const bucket = require('./bucket').bucket;
 const r = require('./bucket').r;
 const stringify = require('safe-stable-stringify');
+const flatten = require('flat')
 
 exports.encapsulate = hepjs.encapsulate;
 exports.decapsulate = hepjs.decapsulate;
 		    
 exports.processHep = function processHep(data,socket) {
 	try {
-	  var decoded = hepjs.decapsulate(data);
-  	  log('%data:cyan HEP [%s:blue]', JSON.stringify(socket) );
-	  switch(decoded.payloadType) {
+	  //var decoded = hepjs.decapsulate(data);
+  	  log('%data:cyan HEP Net [%s:blue]', JSON.stringify(socket) );
+	  try { var decoded = hepjs.decapsulate(data); decoded = flatten(decoded); } catch(e) { log('%s:red',e); }
+	  switch(decoded['rcinfo.payloadType']) {
 		case 1:
 	  	  log('%data:cyan HEP Type [%s:blue]', 'SIP' );
 		  log('%data:cyan HEP Payload [%s:yellow]', stringify( sip.parse(decoded.payload)) );
@@ -21,7 +23,7 @@ exports.processHep = function processHep(data,socket) {
 	  	  log('%data:cyan HEP Type [%s:blue]', decoded.payloadType );
 		  log('%data:cyan HEP Payload [%s:yellow]', stringify(decoded.payload) );
 	  }
-	  if(decoded.rcinfo.timeSeconds) decoded.rcinfo.ts = r.epochTime(decoded.rcinfo.timeSeconds);
+	  if(decoded['rcinfo.timeSeconds']) decoded['rcinfo.ts'] = r.epochTime(decoded['rcinfo.timeSeconds']);
 	  bucket.push(decoded);
 		
 	} catch(err) { log('%error:red %s', err.toString() ) }
