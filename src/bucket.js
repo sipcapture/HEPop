737e918c6aaa8554bc34ba6eb7b5b4132f23f915
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-03-11 15:42:34 +0100

    reformat for multi out

--- src/bucket.js
+++ src/bucket.js
@@ -7,31 +7,56 @@ var config = require('./config').getConfig();
 const log = require('./logger');
 const bucket_emitter = require('./bulk-emitter')
 const stringify = require('safe-stable-stringify');
-var r;
+var r = false;
+var pgp = false;
 
-try {
+var r_bucket = false;
+var p_bucket = false;
+
+// RethinkDB
+if (config.db.rethink){
+ try {
   r = require('./rethink').connect();
   log('%start:green Initialize RethinkDB driver' );
-} catch(e){ log('%stop:red Failed to Initialize RethinkDB driver',e); return; }
 
-log('%start:green Initializing Bulk bucket...');
-const bucket = bucket_emitter.create(config.queue);
+  log('%start:green Initializing Bulk bucket...');
+  r_bucket = bucket_emitter.create(config.queue);
+  r_bucket.on('data', function(data) {
+    // Bulk ready to emit!
+    if (config.debug) log('%data:cyan RethinkDB BULK Out [%s:blue]', stringify(data) );
+    // TODO: add chain emitter to multiple backends or pipelines
+    r.db(config.dbName).table(config.tableName).insert(data).run();
+  }).on('error', function(err) {
+    log('%error:red %s', err.toString() )
+  });
 
-bucket.on('data', function(data) {
-  // Bulk ready to emit!
-  if (config.debug) log('%data:cyan BULK Out [%s:blue]', stringify(data) );
-  // TODO: add chain emitter to multiple backends or pipelines
-  r.db(config.dbName).table(config.tableName).insert(data).run();
-}).on('error', function(err) {
-  log('%error:red %s', err.toString() )
-});
+ } catch(e){ log('%stop:red Failed to Initialize RethinkDB driver/queue',e); return; }
+}
+// PGSql
+if (config.db.pgsql){
+ try {
+  pgp = require('./pgsql');
+  pgp.setTemplate(config.db.pgsql.schema);
+  p_bucket = bucket_emitter.create(config.queue);
+  p_bucket.on('data', function(data) {
+    // Bulk ready to emit!
+    if (config.debug) log('%data:cyan PGSQL BULK Out [%s:blue]', stringify(data) );
+    // TODO: add chain emitter to multiple backends or pipelines
+    pgp.insert(data);
+  }).on('error', function(err) {
+    log('%error:red %s', err.toString() )
+  });
+
+ } catch(e){ log('%stop:red Failed to Initialize PGSql driver/queue',e); return; }
+}
+
 
 process.on('beforeExit', function() {
   bucket.close(function(leftData) {
     if (config.debug) log('%data:red BULK Leftover [%s:blue]', stringify(leftData) );
-    r.db(config.dbName).table(config.tableName).insert(leftData).run();
+    if (r) r.db(config.dbName).table(config.tableName).insert(leftData).run();
   });
 });
 
-exports.bucket = bucket;
+exports.bucket = r_bucket;
 exports.r = r;
