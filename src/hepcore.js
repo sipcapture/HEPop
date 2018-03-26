d55ebe956a7aa827b48590573142fbc0ac4c42bc
root <root@static.22.8.9.5.clients.your-server.de>
2018-03-26 19:32:50 +0200

    sip parsers to new hep type

--- src/hepcore.js
+++ src/hepcore.js
@@ -1,6 +1,6 @@
 const hepjs = require('hep-js');
 const log = require('./logger');
-const sip = require('sip');
+const sipdec = require('sip');
 const r_bucket = require('./bucket').bucket;
 const pgp_bucket = require('./bucket').pgp_bucket;
 const mdb_bucket = require('./bucket').mdb_bucket;
@@ -27,25 +27,30 @@ exports.processHep = function processHep(data,socket) {
 
 	  switch(insert.hep_header.payloadType) {
 		case 1:
-		  try { var sip = sip.parse(decoded.payload); } catch(e) {}
-		  insert.payload.protocol = 'sip';
-		  if (sip && sip.headers) {
-			  var hdr = sip.headers;
-			  if (hdr.call-id) insert.payload.callid = hdr.call-id;
-			  if (hdr.cseq && hdr.cseq.seq) insert.payload.cseq = hdr.cseq.seq;
-			  if (hdr.from) {
-				if (hdr.from.params.uri) insert.payload.from_user = hdr.from.params.uri;
-				if (hdr.from.params.tag) insert.payload.from_tag = hdr.from.params.tag;
+		  try { var sip = sipdec.parse(insert.raw); 
+			  log("SIP HEADERS: %s", stringify(sip), insert.raw);
+
+		  	insert.payload.protocol = 'SIP';
+		  	if (sip && sip.headers) {
+				  var hdr = sip.headers;
+				  if (hdr['call-id']) insert.payload.callid = hdr['call-id'];
+				  if (hdr['user-agent']) insert.payload.uas = hdr['user-agent'];
+				  if (hdr.cseq && hdr.cseq.seq) insert.payload.cseq = hdr.cseq.seq;
+				  if (hdr.from) {
+					if (hdr.from.name || hdr.from.params.uri ) insert.payload.from_user = hdr.from.name || hdr.from.params.uri;
+					if (hdr.from.params.tag) insert.payload.from_tag = hdr.from.params.tag;
+				  }
+				  if (hdr.to) {
+					if (hdr.to.name) insert.payload.to_user = hdr.to.name;
+					if (hdr.to.params.tag) insert.payload.to_tag = hdr.to.params.tag;
+				  }
+				  if (sip.method) insert.payload.method = sip.method;
 			  }
-			  if (hdr.to) {
-				if (hdr.to.params.uri) insert.payload.to_user = hdr.to.params.uri;
-				if (hdr.to.params.tag) insert.payload.to_tag = hdr.to.params.tag;
+	  		  if (config.debug) {
+				log('%data:cyan HEP Type [%s:blue]', 'SIP' );
+			  	log('%data:cyan HEP Payload [%s:yellow]', stringify( insert.payload, null, 2) );
 			  }
-		  }
-	  	  if (config.debug) {
-			log('%data:cyan HEP Type [%s:blue]', 'SIP' );
-		  	log('%data:cyan HEP Payload [%s:yellow]', stringify( insert.payload, null, 2) );
-		  }
+		  } catch(e) { log("%data:red %s",e);}
 		  break;
 		default:
 	  	  if (config.debug) {
