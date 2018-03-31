893410a3ae5654cf3e2ea771058c0692e08a0c86
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-03-31 21:52:49 +0200

    minor fixes, remove debug

--- src/pgsql.js
+++ src/pgsql.js
@@ -109,7 +109,7 @@ exports.insert = function(bulk,id){
 	    .catch(error => {
 	        // error;
 		if (config.debug) log('PGP ERR: %s',error);
-		if (error.includes('does not exist') {
+		if (error.indexOf('does not exist') !== -1) {
 			createTable(id);
 		}
 		return error;
