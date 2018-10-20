8a3962a94c9705da7dd69ccb09cb2aea603f22d1
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-10-20 23:14:34 +0200

    Hook Elasticsearch buckets (#6)
    * Hook Elasticsearch bucket push for testing

--- src/bucket.js
+++ src/bucket.js
@@ -90,11 +90,13 @@ exports.mdb_bucket = m_bucket;
 if (config.db.elastic){
  try {
   var EsBroker = require('./es-broker');
-  var es = EsBroker.create(config.db.elastic);
-  exports.e_bucket = es;
- } catch(e){ log('%stop:red Failed to Initialize Elastic driver/queue',e); return; }
+  e_bucket = EsBroker.create(config.db.elastic);
+  e_bucket.set_id = function(id){ return; }
+ } catch(e){ log('%stop:red Failed to Initialize Elastic Bulk driver/queue',e); return; }
+
 }
 
+exports.e_bucket = e_bucket;
 
 process.on('beforeExit', function() {
   bucket.close(function(leftData) {
