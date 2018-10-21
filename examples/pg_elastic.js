e76b7ee6fbb4fc3a3b14c9eb6dc309f69e909650
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-10-21 12:43:01 +0200

    Update pg_elastic.js

--- examples/pg_elastic.js
+++ examples/pg_elastic.js
@@ -12,33 +12,17 @@
   "tableName": "hep",
   "db": {
 	"rawSize": 8000,
-  	"pgsql":{
-  	  "host": "localhost",
-  	  "port": 5432,
-  	  "user": "homer_user",
-  	  "password": "homer_password"
-  	}
-  },
-  "metrics": {
-	"elastic" : {
+  	"elastic" : {
      	  "target": "http://localhost:9200",
      	  "max_bulk_qtty": 1000,
      	  "max_request_num": 20, 
      	  "index": "hep"
         }
   },
-  "db_off": {
-	"rethink" : {
-	  "servers":[
-	    { "host": "127.0.0.1", "port":28015 }
-  	  ]
-	},
-  	"mongodb":{
-	  "url": "mongodb://localhost:27017/homer"
-	},
+  "metrics": {
 	"influx":{
 		"period": 30000,
-		"expire": 300000, 
+		"expire": 300000,
 		"dbName": "homer",
 		"hostname": "localhost:8086"
 	}
