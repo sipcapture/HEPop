0e99747de059eb56d6d6e31dd2e6d3dd5ae404bf
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-10-21 12:43:43 +0200

    Update pg_rethink.js

--- examples/pg_rethink.js
+++ examples/pg_rethink.js
@@ -12,36 +12,19 @@
   "tableName": "hep",
   "db": {
 	"rawSize": 8000,
-  	"pgsql":{
-  	  "host": "localhost",
-  	  "port": 5432,
-  	  "user": "homer_user",
-  	  "password": "homer_password"
-  	}
-  },
-  "metrics": {
-	"rethink" : {
+  	"rethink" : {
 	  "servers":[
 	    { "host": "127.0.0.1", "port":28015 }
   	  ]
 	}
   },
-  "db_off": { 
+  "metrics": {
 	"influx":{
 		"period": 30000,
-		"expire": 300000, 
+		"expire": 300000,
 		"dbName": "homer",
 		"hostname": "localhost:8086"
-	},
-  	"mongodb":{
-	  "url": "mongodb://localhost:27017/homer"
-	},
-	"elastic" : {
-     	  "target": "http://localhost:9200",
-     	  "max_bulk_qtty": 1000,
-     	  "max_request_num": 20, 
-     	  "index": "hep"
-        }
+	}
   },
   "debug": false
 }
