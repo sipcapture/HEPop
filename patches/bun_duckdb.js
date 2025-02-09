587418ffc8820a00b7fa4dcc3d4d5f277e1fe616
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2025-02-09 01:34:33 +0100

    Update bun_duckdb.js

--- patches/bun_duckdb.js
+++ patches/bun_duckdb.js
@@ -4,7 +4,7 @@ const getRuntimePlatformArch = () => `${process.platform}-${process.arch}`;
  * @throw Error if there isn't any available native binding for the current platform/arch.
  */
 const getNativeNodeBinding = (runtimePlatformArch) => {
-    return require('@duckdb/node-bindings-linux-x64/duckdb.node');
+    return require(`@duckdb/node-bindings-${process.platform}-${process.arch}/duckdb.node`);
 }
 
 module.exports = getNativeNodeBinding(getRuntimePlatformArch());
