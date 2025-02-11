f5c4b814bc3a5658345d1eadb653fc448633c08a
Lorenzo Mangani <lmangani@Raspberry-M1.local>
2025-02-11 12:48:11 +0100

    fix timestamp ingestion

--- hepop.js
+++ hepop.js
@@ -470,6 +470,26 @@ class ParquetBufferManager {
       await this.flush(type);
     }
   }
+
+  async ensureDirectories() {
+    const metadataDir = path.join(this.baseDir, this.writerId);
+    await fs.promises.mkdir(metadataDir, { recursive: true });
+    
+    // Write initial metadata file if it doesn't exist
+    const metadataPath = path.join(metadataDir, 'metadata.json');
+    if (!fs.existsSync(metadataPath)) {
+      const initialMetadata = {
+        writer_id: this.writerId,
+        next_db_id: 0,
+        next_table_id: 0
+      };
+      
+      await fs.promises.writeFile(
+        metadataPath,
+        JSON.stringify(initialMetadata, null, 2)
+      );
+    }
+  }
 }
 
 class CompactionManager {
